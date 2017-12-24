569513ad31fbf006492c219b5bb1961c6f33c83b
HurricanePete <zpete725@gmail.com>
2017-12-24 02:42:49 -0500

    remove of unused variables

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import NavBar from './nav';
 import Bio from './bio';
 import ProjectSection from './projectSection';
-import Contact from './contact';
 import FooterBar from './footer';
 
 import './styles/App.css';
--- src/components/bio.js
+++ src/components/bio.js
@@ -9,11 +9,9 @@ export default function Bio(props) {
 				<h2 className="tc">Hello, I’m Zak. I am a fullstack JavaScript web developer, React fan, and API lover.</h2>
 			</header>
 			<article className="col-8 clear-float m-auto">
-				<span>"{"</span>
 				<div className="avatar-wrapper tc m-auto">
-					<img src={ProfilePic} alt="Profile Photo" />
+					<img src={ProfilePic} alt="Profile" />
 				</div>
-				<span>'}'</span>
 				<p>I am a recent graduate of Thinkful’s Front End Web Development bootcamp. I started coding so that I could be imaginative and see my designs take shape. I kept coding because I became addicted to the thrill a successful debugging session.</p> 
 				<p id="projects">Away from my computer I am an avid runner, an aspiring cook, and DIY project enthusiast. I de-stress by romping around outside, playing indie videogames, or watching any B-list movie I can get my hands on.</p>
 			</article>
