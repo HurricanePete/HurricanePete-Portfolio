9c6314808aa7cd107c846b0c5f4b94b51f3346a0
HurricanePete <zpete725@gmail.com>
2017-12-25 17:12:05 -0500

    remove of unused code

--- src/components/projectsTile.js
+++ src/components/projectsTile.js
@@ -11,7 +11,7 @@ export default class ProjectsTile extends React.Component {
 		const tile = this.props;
 		let repoSection = <li className="dib tc"><a className="" href={tile.repo} target="_blank"><div className="link-button w4">Repo</div></a></li>;
 		if(Array.isArray(tile.repo)) {
-//adds repos if there is front and back-end 
+//adds multiple repo links if there is front and back-end 
 			repoSection = tile.repo.map((item, index) => 
 				<li className="dib tc" key={index}><a className="" href={item} target="_blank"><div className="link-button">{index === 0 ? 'Front-end ' : 'Back-end '}Repo</div></a></li>
 			);
@@ -20,7 +20,6 @@ export default class ProjectsTile extends React.Component {
 			<li className="dib tc fw6 mh3" key={index}>{tech}</li>
 		);
 		return(
-			
 			<article className="project-tile col-8 clear-float m-auto">
 				<Fade bottom big fraction={0.01} duration={700}>
 					<div className="img-wrapper tc m-auto">
