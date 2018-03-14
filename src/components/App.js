90f3ed4e6a42ff8d9acc838452ec8f31cbeba4cd
HurricanePete <zpete725@gmail.com>
2018-03-14 13:09:30 -0400

    fix of bug caused by perspective inheritance on different browsers

--- src/components/App.js
+++ src/components/App.js
@@ -10,15 +10,15 @@ import './styles/App.css';
 import './styles/response-grid.css';
 
 export default class App extends React.Component {
+	// <main>
+	// </main>
 	render() {
 	    return (
 	    	<div className="parallax-wrapper">
 		    	<NavBar />
-		    	<main>
 			        <Intro />
 			        <ProjectSection />
 			        <Bio />
-		      	</main>
 		      	<FooterBar />
 			</div>
 	    );
